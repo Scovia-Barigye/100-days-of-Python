067aab6845bf304288b50e637259d741b0ef0915
Scovia-Barigye <skatusiime05@gmail.com>
2026-01-05 07:11:28 +0300

    create quiz.py
    quiz

---
+++ quiz.py
@@ -0,0 +1,9 @@
+print("Welcome to my quiz!")
+
+answer = input("do you want to play? (yes/no): ").lower()
+if answer != "yes":
+    print("Maybe next time!");
+    quit();
+else:
+    print ("let's play!");
+    
