5907fcaaa5b9132adaecc0c07f583c6299ffa6a8
Scovia-Barigye <skatusiime05@gmail.com>
2026-01-05 08:07:07 +0300

    quiz game

--- quiz.py
+++ quiz.py
@@ -6,4 +6,36 @@ if answer != "yes":
     quit();
 else:
     print ("let's play!");
+
+score=0;
     
+answer =input("what does the CPU stand for? ").lower()
+if answer == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer =input("what does the GUI stand for? ").lower()
+if answer == "graphical user interface":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer =input("what does the PSU stand for? ").lower()
+if answer == "power supply":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer =input("what does the RAM stand for? ").lower()
+if answer == "random access memory":
+    print("correct!") 
+    score += 1
+else:
+    print("incorrect!")
+    
+print("You got " +str(score) + " questions correct!")
+print("You got " +str((score/4)*100) + "%.")
